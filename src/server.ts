884a98ed61a4437c39d8d261cc71a73e587e6a5d
victorstella <victor.stella96@gmail.com>
2021-08-31 21:50:41 -0300

    socket.io implementation

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import 'express-async-errors'
 import express, { json } from 'express'
+import socketio from 'socket.io'
+import http from 'http'
+import path from 'path'
 
 import routes from './routes'
 import rateLimiterRedisMiddleware from './configs/httpRateLimiter'
@@ -8,13 +11,20 @@ import cors from 'cors'
 import errorHandler from './errors/handler'
 
 export const app = express()
+const httpServer = http.createServer(app)
+const io = new socketio.Server(httpServer)
+
+io.on('connection', (socket) => {
+  console.log(`New connection: ${socket.id}`)
+})
 
 app
   .use(rateLimiterRedisMiddleware)
   // cors depois do rate limiter
   .use(cors())
   .use(errorHandler)
+  // .use(express.static(path.resolve(__dirname, '..', 'public')))
   .use(json())
   .use(routes)
 
-app.listen(3333, () => console.log('server running'))
+httpServer.listen(3333, () => console.log('server running'))
